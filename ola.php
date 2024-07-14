e886a56ae40fd87feea7908ae2e980b3cdb5f8ff
ClaudioRibeiro2 <claudio_ribeiro2@estudante.sc.senai.br>
2024-07-14 12:28:42 -0300

    php

--- ola.php
+++ ola.php
@@ -5,6 +5,13 @@
     </head>
     <body>
         <?php echo "<p>Olá Mundo Cruel! Primeiro Código em PHP</p>"; ?>
-	    <?php phpinfo(); ?>
+        
+        <?php if ($expression == true): ?>
+                Isso irá aparecer se a expressão for verdadeira.
+        <?php else: ?>
+                Senão isso irá aparecer.
+        <?php endif; ?>
+
+        <?php phpinfo(); ?>
     </body>
 </html>
